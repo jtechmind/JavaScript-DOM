af884bfa5a7147bc882e47ac265e56ae48135fba
jtechmind <jtechmind@gmail.com>
2024-04-03 23:35:30 +0530

    Backgroud Changer: files modified, background color will change when you click on a div.

--- BackgroundColorChanger.js
+++ BackgroundColorChanger.js
@@ -1,3 +1,4 @@
-document.querySelector("#container").addEventListener("click", (e) => {
-  alert(e.currentTarget);
+document.querySelector("div").addEventListener("click", (e) => {
+  //   alert(e.target.className);
+  document.body.style.background = e.target.className;
 });
