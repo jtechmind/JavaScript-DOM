c2cfb628fe34413e797409b5a986376e193ca95b
jtechmind <jtechmind@gmail.com>
2024-04-04 23:58:56 +0530

    modification in BackgroundColorChanger.

--- BackgroundColorChanger.js
+++ BackgroundColorChanger.js
@@ -1,4 +1,6 @@
 document.querySelector("div").addEventListener("click", (e) => {
-  //   alert(e.target.className);
+  // alert(e.target.className);
+  // alert(e.target.style.backgroundcolor);
+  // alert(window.getComputedStyle(e.target))
   document.body.style.background = e.target.className;
 });
