62f17b639c0527b4cc0a97b59c5292a15a9ce8b5
jtechmind <jtechmind@gmail.com>
2024-04-14 00:04:39 +0530

    project-01 FaceBook: change status on single button click.

--- projects/project-01/script.js
+++ projects/project-01/script.js
@@ -1,13 +1,19 @@
 const requestStatus = document.querySelector("h5");
 const addFriendBtn = document.querySelector("#add");
-const rmFriendBtn = document.querySelector("#remove");
+let isFriend = false;
 
 addFriendBtn.addEventListener("click", function () {
-  requestStatus.innerText = "Friend";
-  requestStatus.style.color = "green";
-});
-
-rmFriendBtn.addEventListener("click", function () {
-  requestStatus.innerText = "You May Know";
-  requestStatus.style.color = "chocolate";
+  if (!isFriend) {
+    requestStatus.innerText = "Friend";
+    requestStatus.style.color = "green";
+    addFriendBtn.innerText = "Remove Friend";
+    addFriendBtn.style.background = "#dadada";
+    isFriend = true;
+  } else {
+    requestStatus.innerText = "You May Know";
+    requestStatus.style.color = "chocolate";
+    addFriendBtn.innerText = "Add Friend";
+    addFriendBtn.style.background = "cadetblue";
+    isFriend = false;
+  }
 });
